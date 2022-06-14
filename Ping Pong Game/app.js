ab66191984e82f946b8cde4eea95a58b4139b28d
Tarun Bisht <bishttarun2002@gmail.com>
2022-06-14 18:27:40 +0530

    Update app.js

--- Ping Pong Game/app.js
+++ Ping Pong Game/app.js
@@ -2,12 +2,12 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const reset = document.querySelector('#reset');
-const value = document.querySelector('#option');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = value;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function() {
@@ -17,6 +17,10 @@ p1Button.addEventListener('click', function() {
         if(p1Score === winningScore)
         {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -29,15 +33,30 @@ p2Button.addEventListener('click', function() {
         if(p2Score === winningScore)
         {
             isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
 })
 
-reset.addEventListener('click', function() {
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset () {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-})
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
