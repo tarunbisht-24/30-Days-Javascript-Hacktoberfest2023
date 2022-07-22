8bf69e4339c4cd66a68debdf5b9ec217b2ce6a63
Tarun Bisht <bishttarun2002@gmail.com>
2022-07-22 20:21:43 +0530

    Update app.js

--- 11 - Custom Video Player/app.js
+++ 11 - Custom Video Player/app.js
@@ -1,1 +1,24 @@
-now tf go study
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    
+    /*if(video.paused) {
+        video.play();
+    }
+    else{
+        video.pause();
+    }*/
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
