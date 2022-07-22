6244059994e09c83896d5e6cad9e999075c7e974
Tarun Bisht <bishttarun2002@gmail.com>
2022-07-22 20:32:03 +0530

    Update app.js

--- 10 - Hold Shift and Check Checkboxes/app.js
+++ 10 - Hold Shift and Check Checkboxes/app.js
@@ -1,26 +1,26 @@
-        const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
-        let lastChecked;
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
 
-        function handleCheck(e)
+function handleCheck(e)
+  {
+     let inBetween = false;
+     if(e.shiftKey && this.checked)
         {
-            let inBetween = false;
-            if(e.shiftKey && this.checked)
-            {
-                checkboxes.forEach(checkbox => {
-                    console.log(checkbox);
-                    if(checkbox === this || checkbox === lastChecked)
-                    {
-                        inBetween = !inBetween;
-                        console.log('Starting to check them in between!');
-                    }
+         checkboxes.forEach(checkbox => {
+         console.log(checkbox);
+         if(checkbox === this || checkbox === lastChecked)
+           {
+            inBetween = !inBetween;
+            console.log('Starting to check them in between!');
+           }
 
-                    if(inBetween)
-                    {
-                        checkbox.checked = true;
-                    }
-                });
-            }
-            lastChecked = this;
-        }
+         if(inBetween)
+           {
+             checkbox.checked = true;
+           }
+       });
+     }
+       lastChecked = this;
+    }
         
-        checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
